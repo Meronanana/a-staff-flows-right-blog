1872a319b1c30bb79c275c4505fcbb50d87d9b30
Jae-Seong Jeong <44183028+Meronanana@users.noreply.github.com>
2024-06-01 17:10:00 +0900

    POST 요청 (api/post) 버그 수정 (#2)
    * :bug: Body parsing 방식 변경
    
    * :bug: &new parsing 방식 변경

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -5,26 +5,32 @@ import path from "path";
 export async function GET(request: Request) {}
 
 export async function POST(request: Request) {
-  const body = await request.body
-    ?.getReader()
-    .read()
-    .then(({ value }) => JSON.parse(value?.toLocaleString() as string));
+  let body;
+  if (request.body) {
+    body = await request.body
+      .getReader()
+      .read()
+      .then(({ value }) => {
+        if (value) {
+          // console.log("value: ", Buffer.from(value).toString("utf-8"));
+          return JSON.parse(Buffer.from(value).toString("utf-8"));
+        }
+      });
+  }
 
   // read markdown file
   let markdown: string;
   try {
-    markdown = fs.readFileSync(
-      path.join(process.cwd(), `./public/assets/posts/${body.slug}.md`),
-      "utf8"
-    );
+    markdown = fs.readFileSync(path.join(process.cwd(), `./public/assets/posts/${body.slug}.md`), "utf8");
   } catch (e) {
+    console.log(e);
     return Response.json({ message: "File not found" }, { status: 404 });
   }
 
   const { data, content } = matter(markdown);
 
   // split with '&new' annotation
-  const splitMd: PostContent = content.split("&new\r\n");
+  const splitMd: PostContent = content.split("\n&new\n");
 
   return Response.json({ data: data, content: splitMd }, { status: 200 });
 }
