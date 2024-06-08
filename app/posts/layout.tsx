583274655ac0fefdbc5184ac14c2343537e67fb6
Meronanana <gik1819@gmail.com>
2024-06-08 13:08:24 +0900

    💄 상하 padding 변경

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -4,7 +4,7 @@ export default function PostLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex flex-column items-center justify-center pt-16 pb-10 min-h-screen font-sans bg-white dark:bg-gray-800 text-black dark:text-white">
+    <main className="flex flex-column items-center justify-center pt-20 pb-14 min-h-screen font-sans bg-white dark:bg-gray-800 text-black dark:text-white">
       {children}
     </main>
   );
