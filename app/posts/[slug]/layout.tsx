7dfdc8aec8ed6dd62341c446de02d7554fba942c
Meronanana <gik1819@gmail.com>
2024-06-08 15:11:11 +0900

    ⚡️ 상수 텍스트 부모 title을 상속 받도록 변경

--- app/posts/[slug]/layout.tsx
+++ app/posts/[slug]/layout.tsx
@@ -11,6 +11,7 @@ export async function generateMetadata(
   { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
+  const parentMetadata = await parent;
   let markdown = fs.readFileSync(
     path.join(process.cwd(), `./public/assets/posts/${params.slug}.md`),
     "utf8"
@@ -19,10 +20,10 @@ export async function generateMetadata(
   const data = matter(markdown).data as PostData;
 
   return {
-    title: `${data.title} - A Staff Flows Right`,
+    title: `${data.title} - ${parentMetadata.title?.absolute}`,
     description: data.description,
     openGraph: {
-      title: `${data.title} - A Staff Flows Right`,
+      title: `${data.title} - ${parentMetadata.title?.absolute}`,
       description: data.description,
       url: `https://asfr.vercel.app/posts/${params.slug}`,
       siteName: "A Staff Flows Right",
