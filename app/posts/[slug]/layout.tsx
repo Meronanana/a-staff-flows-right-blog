5831f4e0c0673384c5badc8a10b29645309bbff4
Meronanana <gik1819@gmail.com>
2024-06-08 14:11:37 +0900

    ✨ 동적 메타데이터 생성

---
+++ app/posts/[slug]/layout.tsx
@@ -0,0 +1,42 @@
+import matter from "gray-matter";
+import { Metadata, ResolvingMetadata } from "next";
+import fs from "fs";
+import path from "path";
+
+interface Props {
+  params: { slug: string };
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  let markdown = fs.readFileSync(
+    path.join(process.cwd(), `./public/assets/posts/${params.slug}.md`),
+    "utf8"
+  );
+
+  const data = matter(markdown).data as PostData;
+
+  return {
+    title: `${data.title} - A Staff Flows Right`,
+    description: data.description,
+    openGraph: {
+      title: `${data.title} - A Staff Flows Right`,
+      description: data.description,
+      url: `https://asfr.vercel.app/posts/${params.slug}`,
+      siteName: "A Staff Flows Right",
+      images: [data.coverImage],
+      locale: "ko_KR",
+      type: "website",
+    },
+  };
+}
+
+export default function PostSlugLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return children;
+}
