732bc6a3d952976f05c5c58ee7e9159e9fd5242d
Meronanana <gik1819@gmail.com>
2024-06-09 17:48:03 +0900

    ⚡️ 다크모드 감지 및 저장 기능 구현

--- components/root/DarkmodeIcon.tsx
+++ components/root/DarkmodeIcon.tsx
@@ -6,8 +6,17 @@ import "@/components/component.css";
 
 export default function DarkmodeIcon() {
   useEffect(() => {
-    const icon = document.getElementById("darkmode-icon-text");
+    const localTheme = window.localStorage.getItem("theme");
 
+    if (!localTheme) {
+      changeColorMode(
+        !window.matchMedia("(prefers-color-scheme: dark)").matches
+      );
+    } else {
+      changeColorMode(localTheme !== "dark");
+    }
+
+    const icon = document.getElementById("darkmode-icon-text");
     if (!icon) return;
 
     // 미리 LayoutHeader만 다크모드/라이트모드로 변경
@@ -54,20 +63,29 @@ export default function DarkmodeIcon() {
 
     icon.addEventListener("click", () => {
       const darkmode = document.body.classList.contains("dark");
-      const layoutHeader = document.getElementById("layout-header");
-
-      if (!layoutHeader) return;
-
-      if (darkmode) {
-        document.body.classList.remove("dark");
-        layoutHeader.classList.remove("dark:bg-white/50", "dark:text-black");
-        layoutHeader.classList.add("dark:text-white");
-      } else {
-        document.body.classList.add("dark");
-        layoutHeader.classList.remove("dark");
-      }
+      changeColorMode(darkmode);
     });
   }, []);
+
+  // 현재 모드를 입력. 반대 모드로 변경됨
+  const changeColorMode = (darkmode: boolean) => {
+    const layoutHeader = document.getElementById("layout-header");
+
+    if (!layoutHeader) return;
+
+    if (darkmode) {
+      document.body.classList.remove("dark");
+      layoutHeader.classList.remove("dark:bg-white/50", "dark:text-black");
+      layoutHeader.classList.add("dark:text-white");
+
+      window.localStorage.setItem("theme", "light");
+    } else {
+      document.body.classList.add("dark");
+      layoutHeader.classList.remove("dark");
+
+      window.localStorage.setItem("theme", "dark");
+    }
+  };
 
   return (
     <button className="font-serif overflow-hidden whitespace-break-spaces h-[1.5em] rounded hover:bg-gray-300/50 ">
